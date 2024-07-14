de79653e1ea0dda9f4240d5e9c79071ae9056231
TaeGeon <163709547+dsakdk@users.noreply.github.com>
2024-07-15 01:35:07 +0900

    Update 바구니 뒤집기.py

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,13 +1,13 @@
 N,M = map(int,input().split())
 basket = []
-for i in range(N):
+for i in range(N): # 바구니 번호 지정
     basket.append(str(i+1))
 for _ in range(M):
-    a,b = map(int,input().split())
+    a,b = map(int,input().split()) # 뒤집을 바구니 범위
     while True:
-        if a >= b :
+        if a >= b : # 범위 중간을 지나가면 중단
             break
-        basket[a-1],basket[b-1]=basket[b-1],basket[a-1]
+        basket[a-1],basket[b-1]=basket[b-1],basket[a-1] # 범위 양끝에서 하나씩 뒤집기
         a += 1
         b -= 1
 result = " ".join(basket)
