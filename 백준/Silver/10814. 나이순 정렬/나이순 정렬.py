17ace500da28464d2befd011c3a085401e21f4a2
TaeGeon <163709547+ahn-taegeon@users.noreply.github.com>
2025-03-14 18:28:51 +0900

    [Silver V] Title: 나이순 정렬, Time: 2868 ms, Memory: 46140 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -2,7 +2,7 @@ N = int(input())
 member = []
 for i in range(N):
     age,name = map(str,input().split())
-    member.append((int(age),name)) # 튜플로 리스트에 저장(나이를 int로 받아야함)
-member.sort(key=lambda x : (x[0])) # 튜플의 0번째(나이)를 기준으로 정렬     
+    member.append((int(age),name))
+member.sort(key=lambda x : (x[0]))    
 for k in member:
     print(k[0],k[1])
